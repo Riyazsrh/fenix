eeb0ffd9a9c13ff7f2f8cd392b465a26fe254470
riyazsrh <riyaz@addonwebsolutions.com>
2023-06-07 13:40:21 +0530

    initial commit

--- src/Components/Home/TrailButton.jsx
+++ src/Components/Home/TrailButton.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const ChatBot = () => {
-  const handleButtonClick = () => { };
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      if (scrollY > 200) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleButtonClick = () => {
+    // Handle button click event here
+  };
 
   return (
-    <button className="trail-button" onClick={handleButtonClick}>
+    <button
+      className={`trail-button${showButton ? " show" : ""}`}
+      onClick={handleButtonClick}
+    >
       Free 7 Day Trail
     </button>
   );
