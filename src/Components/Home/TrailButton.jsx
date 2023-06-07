dc2a80f730b4df1b2a48e139705c946e22f261e2
riyazsrh <riyaz@addonwebsolutions.com>
2023-06-07 13:13:02 +0530

    first commit

--- src/Components/Home/TrailButton.jsx
+++ src/Components/Home/TrailButton.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const ChatBot = () => {
-  const handleButtonClick = () => {};
+  const handleButtonClick = () => { };
 
   return (
-    <button className="chat-bot-button" onClick={handleButtonClick}>
+    <button className="trail-button" onClick={handleButtonClick}>
       Free 7 Day Trail
     </button>
   );
